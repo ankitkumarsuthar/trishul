a7d787fc59872f44b946ccaca0646c1d2dec9a46
Ankit Suthar <ankit@codingowls.in>
2025-01-19 01:09:54 +0530

    IMAEGS

--- resources/views/front/home/partials/certificate_logo.blade.php
+++ resources/views/front/home/partials/certificate_logo.blade.php
@@ -1,36 +1,72 @@
 <div class="container my-5">
     <h2 class="text-center mb-4">{{ trans($lang.'certificate_title_txt') }}</h2>
     <div class="row">
-        <div class="col-md-4 col-sm-6 col-12 mb-3">
+        <div class="col-md-3 col-sm-6 col-12 mb-3">
             <div class="text-center">
-                <img src="{{ asset('front-1/img/FSSI-logo.png') }}" class="img-fluid" alt="Client 1 Logo">
+                <img src="{{ asset('front-1/img/FSSI-logo.png') }}" class="img-fluid" alt="FSSI logo">
             </div>
         </div>
-        <div class="col-md-4 col-sm-6 col-12 mb-3">
+        <div class="col-md-3 col-sm-6 col-12 mb-3">
             <div class="text-center">
-                <img src="{{ asset('front-1/img/HALAL-logo.png') }}" class="img-fluid" alt="Client 2 Logo">
+                <img src="{{ asset('front-1/img/HALAL-logo.png') }}" class="img-fluid" alt="Halal logo">
             </div>
         </div>
-        <div class="col-md-4 col-sm-6 col-12 mb-3">
+        <div class="col-md-3 col-sm-6 col-12 mb-3">
             <div class="text-center">
-                <img src="{{ asset('front-1/img/KOSHER-CERTIFICATE-logo.png') }}" class="img-fluid" alt="Client 3 Logo">
+                <img src="{{ asset('front-1/img/KOSHER-CERTIFICATE-logo.png') }}" class="img-fluid" alt="KOSHER Logo">
             </div>
         </div>
-        <div class="col-md-4 col-sm-6 col-12 mb-3">
+        <div class="col-md-3 col-sm-6 col-12 mb-3">
             <div class="text-center">
-                <img src="{{ asset('front-1/img/logo.png') }}" class="img-fluid" alt="Client 4 Logo">
+                <img src="{{ asset('front-1/img/APEDA.png') }}" class="img-fluid" alt="APEDA Logo">
             </div>
         </div>
-        <div class="col-md-4 col-sm-6 col-12 mb-3">
+        <div class="col-md-3 col-sm-6 col-12 mb-3">
             <div class="text-center">
-                <img src="{{ asset('front-1/img/logo.png') }}" class="img-fluid" alt="Client 5 Logo">
+                <img src="{{ asset('front-1/img/GFREE.png') }}" class="img-fluid" alt="Gluten Free Logo">
             </div>
         </div>
-        <div class="col-md-4 col-sm-6 col-12 mb-3">
+        <div class="col-md-3 col-sm-6 col-12 mb-3">
             <div class="text-center">
-                <img src="{{ asset('front-1/img/logo.png') }}" class="img-fluid" alt="Client 6 Logo">
+                <img src="{{ asset('front-1/img/haccp.png') }}" class="img-fluid" alt="HACCP Logo">
             </div>
         </div>
+
+        <div class="col-md-3 col-sm-6 col-12 mb-3">
+            <div class="text-center">
+                <img src="{{ asset('front-1/img/spices_board_india.png') }}" class="img-fluid" alt="Spice board india Logo">
+            </div>
+        </div>
+
+         <div class="col-md-3 col-sm-6 col-12 mb-3">
+            <div class="text-center">
+                <img src="{{ asset('front-1/img/usda.png') }}" class="img-fluid" alt="USDA Logo">
+            </div>
+        </div>
+
+        <div class="col-md-3 col-sm-6 col-12 mb-3">
+            <div class="text-center">
+                <img src="{{ asset('front-1/img/india-organic.png') }}" class="img-fluid" alt="india-organic Logo">
+            </div>
+        </div>
+
+        <div class="col-md-3 col-sm-6 col-12 mb-3">
+            <div class="text-center">
+                <img src="{{ asset('front-1/img/eco-cart.png') }}" class="img-fluid" alt="ECO CART Logo">
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-12 mb-3">
+            <div class="text-center">
+                <img src="{{ asset('front-1/img/iso.png') }}" class="img-fluid" alt="ISO Logo">
+            </div>
+        </div>
+
+        <div class="col-md-3 col-sm-6 col-12 mb-3">
+            <div class="text-center">
+                <img src="{{ asset('front-1/img/gmo-free.png') }}" class="img-fluid" alt="GMO Free Logo">
+            </div>
+        </div>
+
     </div>
 </div>
 
